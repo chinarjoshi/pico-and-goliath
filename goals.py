5b0ede93623c760d042024f227df805bc44e739b
Chinar Joshi <68311366+chinarjoshi@users.noreply.github.com>
2021-03-06 12:51:59 -0500

    Delete goals.py

--- goals.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-
-class Goal:
-    def __init__(self, image, position, velocity):
-        self.image = pygame.image.load(f'images/{image}')
-        self.position = position
-        self.velocity = velocity
-    def accelerate(self):
