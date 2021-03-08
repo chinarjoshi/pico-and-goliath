6d71ca0eac94d68be1a5f4cfabebd664bfe30296
Chinar Joshi <68311366+chinarjoshi@users.noreply.github.com>
2021-03-08 10:29:06 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,9 +2,9 @@
 Pico and Goliath,
     By Chinar Joshi
 
-This is a 2-player competitive arcade game created with Pygame.
+This is a 2-player competitive arcade game created with Pygame and Pygame_Menu.
 
-This package contains helper modules for the main.py file.
+This package contains helper modules for the main.py module.
 For more information, please view README.md
 
 """
